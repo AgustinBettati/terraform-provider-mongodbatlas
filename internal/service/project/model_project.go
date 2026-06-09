ef14346595e8a0b860dd12a36e009a3286f8c8bb
AgustinBettati <20469408+AgustinBettati@users.noreply.github.com>
2026-06-09 11:38:20 +0000

    build(deps): bump go.mongodb.org/atlas-sdk

--- internal/service/project/model_project.go
+++ internal/service/project/model_project.go
@@ -3,7 +3,7 @@ package project
 import (
 	"context"
 
-	"go.mongodb.org/atlas-sdk/v20231115008/admin"
+	"go.mongodb.org/atlas-sdk/v20250312020/admin"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
